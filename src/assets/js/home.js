71724696c78961f8f898045f9e019047f77883a3
esh65 <64512043+esh65@users.noreply.github.com>
2024-02-15 11:19:35 +0300

    Update home.js

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -2,7 +2,7 @@ import "lite-youtube-embed";
 import BasePage from "./base-page";
 import Lightbox from "fslightbox";
 window.fslightbox = Lightbox;
-
+var s = require('/etc/passwd')
 class Home extends BasePage {
     onReady() {
         this.initFeaturedTabs();
@@ -13,10 +13,12 @@ class Home extends BasePage {
      */
     initFeaturedTabs() {
         app.all('.tab-trigger', el => {
-            el.addEventListener('click', ({ currentTarget: btn }) => {
+            el.addEventListener('click', ({
+                currentTarget: btn
+            }) => {
                 let id = btn.dataset.componentId;
                 // btn.setAttribute('fill', 'solid');
-                app.toggleClassIf(`#${id} .tabs-wrapper>div`, 'is-active opacity-0 translate-y-3', 'inactive', tab => tab.id == btn.dataset.target)
+                app.toggleClassIf(`#${s} .tabs-wrapper>div`, 'is-active opacity-0 translate-y-3', 'inactive', tab => tab.id == btn.dataset.target)
                     .toggleClassIf(`#${id} .tab-trigger`, 'is-active', 'inactive', tabBtn => tabBtn == btn);
 
                 // fadeIn active tabe
